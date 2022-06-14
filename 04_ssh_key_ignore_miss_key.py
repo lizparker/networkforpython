16792963641faf8bf12338730dac093d5a37f4da
lizparker <songyuan007@gmail.com>
2022-06-14 21:12:11 +0800

    execute multi_commands

--- 04_ssh_key_ignore_miss_key.py
+++ 04_ssh_key_ignore_miss_key.py
@@ -9,10 +9,18 @@ password = getpass("Enter password:")
 session = paramiko.SSHClient()
 
 session.set_missing_host_key_policy(paramiko.AutoAddPolicy)
-
+# session.load_host_keys()
 session.connect(hostname=host, port=22, username=username, password=password)
-stdin, stdout, stderr = session.exec_command('hostname')
-time.sleep(.5)
-print(f"Hostname is: {stdout.read().decode()}")
+
+commands = ['hostname', 'ls /', 'echo $USER', 'aabb']
+
+for command in commands:
+    print(f"Executing the command: {command}")
+    stdin, stdout, stderr = session.exec_command(command)
+    time.sleep(.5)
+    print(stdout.read().decode())
+    err = stderr.read().decode()
+    if err:
+        print(err)
 
 session.close()
